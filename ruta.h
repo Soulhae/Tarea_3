50d3a854f343f477a59406541d5e6686675e8358
eyl-lana <eyleen.sanchez.g@mail.pucv.cl>
2021-05-15 19:59:11 -0400

    leer archivo (falta guardar los datos en grafos)

---
+++ ruta.h
@@ -0,0 +1,13 @@
+#ifndef RUTA_H
+#define RUTA_H
+
+typedef struct Coordenadas Coordenadas;
+
+void bienvenida();
+
+void leer_archivo();
+
+void guardar_coord(int num_entrega, int coord_x, int coord_y);
+
+#endif /* RUTA_H */
+
